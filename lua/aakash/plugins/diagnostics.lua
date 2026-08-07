95d0cd548cc4a43027cf78e083628d80391aeeb6
Aakash Kumar <aakashinthesky@gmail.com>
2026-08-06 20:22:38 -0700

    style: restore diagnostic config formatting

--- lua/aakash/plugins/diagnostics.lua
+++ lua/aakash/plugins/diagnostics.lua
@@ -1,28 +1,28 @@
 return {
-	-- Trouble
-	{
-		"folke/trouble.nvim",
-		cmd = { "Trouble" },
-		opts = {},
-		keys = {
-			{
-				"<leader>xx",
-				"<cmd>Trouble diagnostics toggle<CR>",
-				desc = "Diagnostics: project",
-			},
-			{
-				"<leader>xX",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-				desc = "Diagnostics: current buffer",
-			},
-		},
-	},
+  -- Trouble
+  {
+    'folke/trouble.nvim',
+    cmd = { 'Trouble' },
+    opts = {},
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<CR>',
+        desc = 'Diagnostics: project',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<CR>',
+        desc = 'Diagnostics: current buffer',
+      },
+    },
+  },
 
-	{
-		"folke/which-key.nvim",
-		opts = function(_, opts)
-			opts.spec = opts.spec or {}
-			table.insert(opts.spec, { "<leader>x", group = "Diagnostics" })
-		end,
-	},
+  {
+    'folke/which-key.nvim',
+    opts = function(_, opts)
+      opts.spec = opts.spec or {}
+      table.insert(opts.spec, { '<leader>x', group = 'Diagnostics' })
+    end,
+  },
 }
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,41 +7,41 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic Config & Keymaps
 --  See `:help vim.diagnostic.Opts`
-vim.diagnostic.config({
-	update_in_insert = false,
-	severity_sort = true,
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
 
-	float = { border = "rounded", source = "if_many" },
-	signs = {
-		severity = { min = vim.diagnostic.severity.WARN },
-		text = {
-			[vim.diagnostic.severity.ERROR] = "●",
-			[vim.diagnostic.severity.WARN] = "●",
-		},
-	},
-	underline = { severity = { min = vim.diagnostic.severity.WARN } },
+  float = { border = 'rounded', source = 'if_many' },
+  signs = {
+    severity = { min = vim.diagnostic.severity.WARN },
+    text = {
+      [vim.diagnostic.severity.ERROR] = '●',
+      [vim.diagnostic.severity.WARN] = '●',
+    },
+  },
+  underline = { severity = { min = vim.diagnostic.severity.WARN } },
 
-	virtual_text = false,
-	virtual_lines = false,
+  virtual_text = false,
+  virtual_lines = false,
 
-	jump = {
-		on_jump = function(_, bufnr)
-			vim.diagnostic.open_float({
-				bufnr = bufnr,
-				scope = "cursor",
-				focus = false,
-			})
-		end,
-	},
-})
+  jump = {
+    on_jump = function(_, bufnr)
+      vim.diagnostic.open_float {
+        bufnr = bufnr,
+        scope = 'cursor',
+        focus = false,
+      }
+    end,
+  },
+}
 
-require("aakash.diagnostic_ui").setup()
+require('aakash.diagnostic_ui').setup()
 
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -49,7 +49,7 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -61,10 +61,10 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -78,10 +78,8 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("aakash-highlight-yank", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('aakash-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
 })
