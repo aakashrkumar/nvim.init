b9890ad9f01c69e8a2f8a5b7a315f5001d482fd1
Aakash Kumar <aakashinthesky@gmail.com>
2026-08-06 19:59:59 -0700

    feat: calm diagnostic presentation

--- lua/aakash/plugins/diagnostics.lua
+++ lua/aakash/plugins/diagnostics.lua
@@ -1,46 +1,28 @@
 return {
-  {
-    'rachartier/tiny-inline-diagnostic.nvim',
-    event = 'VeryLazy',
-    opts = {
-      preset = 'modern',
+	-- Trouble
+	{
+		"folke/trouble.nvim",
+		cmd = { "Trouble" },
+		opts = {},
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<CR>",
+				desc = "Diagnostics: project",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+				desc = "Diagnostics: current buffer",
+			},
+		},
+	},
 
-      signs = {
-        diag = '',
-      },
-
-      options = {
-        use_icons_from_diagnostic = false,
-        override_open_float = true,
-        multilines = { enabled = true },
-      },
-    },
-  },
-
-  -- Trouble
-  {
-    'folke/trouble.nvim',
-    cmd = { 'Trouble' },
-    opts = {},
-    keys = {
-      {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<CR>',
-        desc = 'Diagnostics: project',
-      },
-      {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<CR>',
-        desc = 'Diagnostics: current buffer',
-      },
-    },
-  },
-
-  {
-    'folke/which-key.nvim',
-    opts = function(_, opts)
-      opts.spec = opts.spec or {}
-      table.insert(opts.spec, { '<leader>x', group = 'Diagnostics' })
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		opts = function(_, opts)
+			opts.spec = opts.spec or {}
+			table.insert(opts.spec, { "<leader>x", group = "Diagnostics" })
+		end,
+	},
 }
